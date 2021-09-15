b816a514c074ad5a4708af6207194dee4c3dc1f6
Kaled Sánchez <kaledsv@gmail.com>
2021-09-15 14:39:11 -0600

    Create StringParser.java

---
+++ app/src/main/java/com/jacob/educamep/clasesLogicas/StringParser.java
@@ -0,0 +1,17 @@
+package com.jacob.educamep.clasesLogicas;
+
+import java.util.ArrayList;
+
+public class StringParser {
+    public ArrayList<String[]> convert (String line){
+        ArrayList<String[]> list = new ArrayList<String[]>();
+        if (!line.equals("")){
+            String[] rows = line.split("-", 0);
+            for (int i=0; i<rows.length; i++){
+                String[] row = rows[i].split(";", 0);
+                list.add(row);
+            }
+        }
+        return list;
+    }
+}
