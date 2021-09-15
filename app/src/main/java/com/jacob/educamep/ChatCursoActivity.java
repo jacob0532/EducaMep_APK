b40002a94d9672a4d0e7fb7c68a245143c0ad1cf
Kaled Sánchez <kaledsv@gmail.com>
2021-09-14 18:40:32 -0600

    Chat del curso
    interfaz y actividad para el chat del curso

---
+++ app/src/main/java/com/jacob/educamep/ChatCursoActivity.java
@@ -0,0 +1,14 @@
+package com.jacob.educamep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatCursoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chatcurso);
+    }
+}
