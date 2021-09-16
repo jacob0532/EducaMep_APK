754edf860c80a67ea9352260271e1cda39d4552d
jacob0532 <61889488+jacob0532@users.noreply.github.com>
2021-09-16 10:28:57 -0600

    Spinner en agregar curso

--- app/src/main/java/com/jacob/educamep/AgregarCursoActivity.java
+++ app/src/main/java/com/jacob/educamep/AgregarCursoActivity.java
@@ -3,13 +3,17 @@ package com.jacob.educamep;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TextView;
 
-public class AgregarCursoActivity extends AppCompatActivity {
+public class AgregarCursoActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +30,22 @@ public class AgregarCursoActivity extends AppCompatActivity {
         final Button btnAgregarHora = findViewById(R.id.btnAgregarHora);
         final Button btnAtras = findViewById(R.id.btnAtras);
         final Button btnConfirmar = findViewById(R.id.btnConfirmar);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.diasSpinner, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        comboBoxDia.setAdapter(adapter);
+        comboBoxDia.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String text = comboBoxDia.getSelectedItem().toString();
+                Log.d("TEXTO DEL DIA",text);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/jacob/educamep/clasesLogicas/StringParser.java
+++ app/src/main/java/com/jacob/educamep/clasesLogicas/StringParser.java
@@ -6,7 +6,7 @@ public class StringParser {
     public ArrayList<String[]> convert (String line){
         ArrayList<String[]> list = new ArrayList<String[]>();
         if (!line.equals("")){
-            String[] rows = line.split("-", 0);
+            String[] rows = line.split("#", 0);
             for (int i=0; i<rows.length; i++){
                 String[] row = rows[i].split(";", 0);
                 list.add(row);
