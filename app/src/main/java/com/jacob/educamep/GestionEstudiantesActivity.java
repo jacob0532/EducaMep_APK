9eb3c280f18e54d204e4131b11de535d46f2744f
JacobPG <51519160+JacobPG@users.noreply.github.com>
2021-09-16 22:38:01 -0600

    Modificar Estudiante

--- app/src/main/java/com/jacob/educamep/GestionEstudiantesActivity.java
+++ app/src/main/java/com/jacob/educamep/GestionEstudiantesActivity.java
@@ -51,6 +51,31 @@ public class GestionEstudiantesActivity extends AppCompatActivity {
                 }
             }
         });
+        
+        btnModificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Prueba
+                Administrador usuario = new Administrador(123456, "pepito", "Ramirez", "Mora", "pepito@gmail.com", "superpepito");
+                BDEducaMep db = new BDEducaMep(GestionEstudiantesActivity.this, (Usuario) usuario, 3, 2);
+                db.execute();
+                //Estudiante usuario = (Estudiante) getIntent().getSerializableExtra("usuario");
+                //BDEducaMep db = new BDEducaMep(view.getContext(), usuario, 4, 1);
+                //db.execute((int) getIntent().getSerializableExtra("idCurso"));
+                while(db.resultado2 == null){
+                    Log.d("MI OPPPOPOPO", "loading...");
+                }
+                if (db.resultado2.isEmpty()){
+                    Toast.makeText(view.getContext(), "no hay un profesor asignado",Toast.LENGTH_LONG).show();
+                }else{
+                    Intent send = new Intent(view.getContext(),ModificarEstudianteActivity.class);
+                    Bundle b = new Bundle();
+                    b.putSerializable("estudiantes", (ArrayList<String[]>)db.resultado2);
+                    send.putExtras(b);
+                    startActivity(send);
+                }
+            }
+        });
 
     }
 }
