2bd34e502178bf31d0a31db2f9a1b842ce0c6750
Kaled Sánchez <kaledsv@gmail.com>
2021-11-12 20:50:49 -0600

    Crear noticia docente

---
+++ weaWeb/Docente/includes/navigation.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+  <a href="index.php" class="navbar-brand">EducaMEP</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="asignarTarea.php">Asignar Tarea</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="publicarNotica.php">Publicar Noticia</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="listaEstudiantes.php">Lista de Estudiantes</a>
+            </li>
+        </ul>
+        <div class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Sign Up</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Login</a>
+            </li>
+        </div>
+    </div>
+  </div>
+</nav>
---
+++ weaWeb/Docente/index.php
@@ -0,0 +1,5 @@
+<?php ?>
+
+<?php include("includes/header.php") ?>
+    <h1>Prueba</h1>
+<?php include("includes/footer.php") ?>
